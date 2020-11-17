38e59154ee094c7d8482a79d1abe55f1509f960c
Eduardo Cuevas <eduardo.cuevas@sertres.com>
2020-11-17 17:30:10 -0600

    primer commit API REST sertres reporte quincenal

---
+++ src/main/java/com/sertres/reporte/SertresReporteApplication.java
@@ -0,0 +1,13 @@
+package com.sertres.reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertresReporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertresReporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sertres/reporte/SertresReporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sertres.reporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SertresReporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
